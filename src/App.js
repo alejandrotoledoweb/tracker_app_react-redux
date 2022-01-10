a0ed7a56398d1f4543e9a462f23993e84d636850
Alejandro Toledo <atoledofr@gmail.com>
2022-01-10 14:41:43 -0500

    setup react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/navbar.component';
+import ExcercisesList from './components/exercises-list.component';
+import EditExcercise from './components/edit-exercise.component';
+import CreateExercise from './components/create-exercise.component';
+import CreateUser from './components/create-user.component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Navbar />
+        <br />
+        <Route path="/" exact component={<ExcercisesList />} />
+        <Route path="/edit/:id" exact component={<EditExcercise />} />
+        <Route path="/create" exact component={<CreateExercise />} />
+        <Route path="/user" exact component={<CreateUser />} />
+      </Routes>
+    </Router>
   );
 }
 
